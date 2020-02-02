3d1a2548393e05ed0b15010ee27127f34ed031e7
stembrino <stembrino@gmail.com>
2020-02-02 19:11:13 +0000

    update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Paylog'`, () => {
+  it(`should have as title 'MudaT'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Paylog');
+    expect(app.title).toEqual('MudaT');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Paylog app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('MudaT app is running!');
   });
 });
